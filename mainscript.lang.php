7c89270f242e9b57814becddb817de23f2389bb6
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2007-03-09 11:40:45 +0000

    JavaScript String ' 衝突修正
    git-svn-id: http://svn.openfoundry.org/pixmicat/PIO@364 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mainscript.lang.php
+++ mainscript.lang.php
@@ -3,4 +3,4 @@ include_once('./config.php'); // 引入設定檔
 include_once('./lib_language.php'); // 引入語系
 header('Content-Type: text/javascript');
 ?>
-var msgs=['<?php echo _T('regist_withoutcomment'); ?>','<?php echo _T('regist_upload_notsupport'); ?>','<?php echo _T('js_convert_sakura'); ?>'];
+var msgs=['<?php echo str_replace('\'', '\\\'', _T('regist_withoutcomment')); ?>','<?php echo str_replace('\'', '\\\'', _T('regist_upload_notsupport')); ?>','<?php echo str_replace('\'', '\\\'', _T('js_convert_sakura')); ?>'];
