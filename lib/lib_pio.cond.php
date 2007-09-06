4fd1d775530f4a8bdb619b51483f2c28049b23fb
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2007-09-06 11:51:20 +0000

    -[PIO] 新增 ByThreadAliveTimeCondition 以討論串發文天數作為刪除判斷 (超過指定天數就砍，最大砍到剩一篇)
    git-svn-id: http://svn.openfoundry.org/pixmicat/PIO@523 2a703577-9a0d-0410-ac02-84028a1de3c1

--- lib/lib_pio.cond.php
+++ lib/lib_pio.cond.php
@@ -34,4 +34,28 @@ class ByThreadCountCondition{
 		return $PIO->fetchThreadList(intval($limit * ($type=='predict' ? 0.95 : 1)), $limit);
 	}
 }
+
+/* 以討論串生存時間作為刪除判斷 */
+class ByThreadAliveTimeCondition{
+	/*public static */function check($type, $limit){
+		global $PIO;
+		$oldestThreadNo = $PIO->fetchThreadList($PIO->threadCount() - 1, 1, true); // 最舊討論串編號
+		$oldestThread = $PIO->fetchPosts($oldestThreadNo);
+		return (time() - substr($oldestThread[0]['tim'], 0, 10) >= 86400 * $limit * ($type=='predict' ? 0.95 : 1));
+	}
+
+	/*public static */function listee($type, $limit){
+		global $PIO;
+		$ThreadNo = $PIO->fetchThreadList(0, 0, true); sort($ThreadNo); // 討論串編號陣列 (由舊到新)
+		$NowTime = time();
+		$i = 0;
+		foreach($ThreadNo as $t){
+			$post = $PIO->fetchPosts($t);
+			if($NowTime - substr($post[0]['tim'], 0, 10) < 86400 * $limit * ($type=='predict' ? 0.95 : 1)) break; // 時間不符合
+			$i++;
+		}
+		if(count($ThreadNo)===$i){ $i--; } // 保留最新的一篇避免全部刪除
+		return array_slice($ThreadNo, 0, $i);
+	}
+}
 ?>
