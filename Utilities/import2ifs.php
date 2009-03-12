c03ef41e34fcf53f7e27b406c4d66493a5b82676
roytam1 <roytam1@2a703577-9a0d-0410-ac02-84028a1de3c1>
2009-03-12 14:31:54 +0000

    + [PIO][Utilities] import2ifs
    git-svn-id: http://svn.openfoundry.org/pixmicat/PIO@687 2a703577-9a0d-0410-ac02-84028a1de3c1

---
+++ Utilities/import2ifs.php
@@ -0,0 +1,22 @@
+<?php
+
+include('./config.php');
+
+include('./lib/fileio/ifs.php');
+
+
+$fio = new IndexFS(FILEIO_INDEXLOG);
+
+$fio->openIndex();
+clearstatcache();
+$dirs = array(IMG_DIR, THUMB_DIR);
+foreach ($dirs as $dir) {
+	if ($handle = opendir($dir)) {
+		while (false !== ($file = readdir($handle))) {
+			if ($file == "." || $file == "..")  continue;
+			$fio->addRecord($file,filesize($dir.'/'.$file),'');
+		}
+	}
+}
+$fio->saveIndex();
+echo "done";
