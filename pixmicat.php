e0712259335d4bb7c20e52944156feb86a57bc2f
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2009-05-16 07:19:59 +0000

    -[pixmicat] 新增依特定類別標籤篩選觀看文章時，能根據No.連結找到文章的完整討論串位置
    git-svn-id: http://svn.openfoundry.org/pixmicat/PIO@696 2a703577-9a0d-0410-ac02-84028a1de3c1

--- pixmicat.php
+++ pixmicat.php
@@ -1,5 +1,5 @@
 <?php
-define("PIXMICAT_VER", 'Pixmicat!-PIO 4th.Release.4-dev (b090425)'); // 版本資訊文字
+define("PIXMICAT_VER", 'Pixmicat!-PIO 4th.Anniversary (v090521)'); // 版本資訊文字
 /*
 Pixmicat! : 圖咪貓貼圖版程式
 http://pixmicat.openfoundry.org/
