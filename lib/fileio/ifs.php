e6131311c3b6710b780132b13875e7259ba4702b
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2012-11-19 14:33:08 +0000

    ifs.php !: [#39172] 修正因 register_shutdown_function 造成回寫 log 內容時工作目錄跑掉的問題
    git-svn-id: http://svn.openfoundry.org/pixmicat/PIO@831 2a703577-9a0d-0410-ac02-84028a1de3c1

--- lib/fileio/ifs.php
+++ lib/fileio/ifs.php
@@ -14,7 +14,7 @@ class IndexFS{
 
 	/* 建構元 */
 	function IndexFS($logfile){
-		$this->logfile = rtrim(dirname('.'), '/\\').DIRECTORY_SEPARATOR.$logfile; // 索引記錄檔位置
+		$this->logfile = realpath(rtrim(dirname('.'), '/\\').DIRECTORY_SEPARATOR.$logfile); // 索引記錄檔位置
 	}
 
 	/* 初始化 */
