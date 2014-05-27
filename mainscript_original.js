1e2f7fc7bba8f70a0ade1bcad85ab09e6c09dd3d
KennyL <apple@apples-MacBook-Pro.local>
2014-05-28 00:40:29 +0800

    fix jpeg javascript bug by me

--- mainscript_original.js
+++ mainscript_original.js
@@ -87,7 +87,7 @@ function c(){
 		if(upfilevalue){
 			ext_allowed = 0; ext_length = ext.length;
 			for(j = 0; j < ext_length; j++){
-				if(upfilevalue.substr(upfilevalue.lastIndexOf('.')).toUpperCase()===ext[j]){
+				if(upfilevalue.substr(upfilevalue.lastIndexOf('.')+1).toUpperCase()===ext[j]){
 					ext_allowed = 1;
 					break;
 				}
