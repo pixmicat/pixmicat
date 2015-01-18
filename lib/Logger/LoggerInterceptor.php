e56e80514c2a19aaab7d84d4bfe81e1f2817f238
Scribe Huang <scribetw+github@gmail.com>
2015-01-18 20:22:39 +0800

    [Issue #25] Added namespace for Pixmicat\Logger. (Must be replaced by PSR-3 logger in the future)

---
+++ lib/Logger/LoggerInterceptor.php
@@ -0,0 +1,43 @@
+<?php
+namespace Pixmicat\Logger;
+
+use Pixmicat\Aop\MethodInterceptor;
+
+/**
+ * Logger Around Advice Interceptor
+ *
+ * @package PMCLibrary
+ * @version $Id$
+ * @since 7th.Release
+ */
+class LoggerInterceptor implements MethodInterceptor
+{
+    private $LOG;
+
+    public function __construct(ILogger $logger)
+    {
+        $this->setLogger($logger);
+    }
+
+    private function setLogger(ILogger $logger)
+    {
+        $this->LOG = $logger;
+    }
+
+    public function invoke(array $callable, array $args)
+    {
+        $result = null;
+        $methodName = $callable[1];
+        $this->LOG->info('Executing %s method', $methodName);
+        $this->LOG->debug('Args: %s', $args);
+
+        try {
+            $result = \call_user_func_array($callable, $args);
+        } catch (\Exception $e) {
+            $this->LOG->error('[%s] %s', $methodName, $e);
+        }
+
+        $this->LOG->debug('Return: %s', $result);
+        return $result;
+    }
+}
