e77431bd0982d2480841024f4968e614a2530a2c
Scribe Huang <scribetw+github@gmail.com>
2015-03-15 23:35:19 +0800

    Follow PSR (Use auto format)

--- default.php
+++ default.php
@@ -1,24 +1,26 @@
 <?php
+namespace Pixmicat;
+
 define('PHP_SELF', 'default.php');
 require './config.php';
-require ROOTPATH . 'lib/PMCLibrary.php';
-require ROOTPATH . 'lib/lib_errorhandler.php';
+require ROOTPATH . 'vendor/autoload.php';
 require ROOTPATH . 'lib/lib_compatible.php';
 require ROOTPATH . 'lib/lib_common.php';
 
 /**
  * 程式首次執行之初始化
- 
+ *
  * @return boolean 是否無錯誤完成
  */
-function init() {
+function init()
+{
     $PIO = PMCLibrary::getPIOInstance();
     $FileIO = PMCLibrary::getFileIOInstance();
 
     if (file_exists(PHP_SELF2)) {
         return true;
     }
-    
+
     if (!is_writable(STORAGE_PATH)) {
         error(_T('init_permerror'));
         return false;
@@ -27,10 +29,12 @@ function init() {
     createDirectories();
     $PIO->dbInit();
     $FileIO->init();
+
     return true;
 }
 
-function createDirectories() {
+function createDirectories()
+{
     $chkfolder = array(
         IMG_DIR,
         THUMB_DIR,
@@ -45,7 +49,8 @@ function createDirectories() {
     }
 }
 
-function redirect() {
+function redirect()
+{
     header("HTTP/1.1 301 Moved Permanently");
     header("Location: pixmicat.php");
 }
